5aa88b6c7550d9c17735dc03f1302b34f9c16fec
FranciscoJavi90 <javiercajas90@gmail.com>
2023-10-15 23:19:14 -0600

    FN adicionales

--- Binmuebles/resources/views/register.blade.php
+++ Binmuebles/resources/views/register.blade.php
@@ -6,6 +6,32 @@
     <title>Registro</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/animate.css@4.1.1/animate.min.css" rel="stylesheet">
+    <style>
+        /* Personaliza los estilos aquí */
+        body {
+            background-color: #f8f9fa;
+        }
+
+        .card {
+            background-color: rgba(255, 140, 0, 0.2); /* Fondo naranja suave */
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+            padding: 20px;
+        }
+
+        h2 {
+            color: #ff8c00; /* Texto naranja */
+        }
+
+        .form-control {
+            border: 1px solid #ff8c00; /* Bordes naranjas */
+        }
+
+        .btn-primary {
+            background-color: #ff8c00; /* Botón naranja */
+            color: #fff; /* Texto blanco */
+        }
+    </style>
 </head>
 <body> 
     <div class="container">
@@ -13,7 +39,7 @@
             <div class="col-md-6">
                 <div class="card">
                     <div class="card-body">
-                        <h2 class="card-title text-center animate__animated animate__fadeInDown">Registro</h2>
+                        <h2 class="card-title text-center animate__animated animate__fadeInDown" style="color: #ff8c00;">Registro</h2>
                         @if(Session::has('registration_success'))
                         <div class="alert alert-success mt-3 animate__animated animate__fadeInUp">
                             {{ Session::get('registration_success') }}
@@ -28,7 +54,7 @@
                             </div>
                             @endif
                             <div class="mb-3">
-                                <label for="emailInput" class="form-label">Email</label>
+                                <label for "emailInput" class="form-label">Email</label>
                                 <input type="email" class="form-control" id="emailInput" name="email" required autocomplete="off">
                             </div>
                             <div class="mb-3">
