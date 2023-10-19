0db8c6a6adf4fa49164ef1521258941b72891b30
FranciscoJavi90 <javiercajas90@gmail.com>
2023-10-18 18:55:43 -0600

    COMMIT ACERCANOSOTROS

--- Binmuebles/resources/views/acercanosotros.blade.php
+++ Binmuebles/resources/views/acercanosotros.blade.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('acercadenosotros.css') }}"> <!-- Puedes crear un archivo de estilos específico para esta página -->
+    <link rel="stylesheet" href="{{ asset('acercadenosotros.css') }}">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <title>Acerca de Nosotros - Binmueble</title>
 </head>
@@ -20,17 +20,32 @@
             </nav>
         </div>
     </header>
-    <main id="acercanosotros" class="container">
-        <div class="acercanosotros-container" style="text-align: center;">
-            <h1>Acerca de nosotros</h1>
-            <p>Somos una empresa ficticia dedicada a brindar soluciones tecnológicas innovadoras.</p>
-            <p>Nuestro equipo está compuesto por profesionales apasionados por la tecnología y comprometidos con la excelencia.</p>
-            <p>¡Contáctanos para conocer más sobre nuestros servicios y proyectos!</p>
-            <div class="acercanosotros-logo">
-                <img src="{{ asset('img/LOGO.png') }}" alt="Logo de la Empresa" style="max-width: 150px;">
-            </div>
+
+    <section id="acercanosotros">
+        <div class="container">
+            <h1>Acerca de Nosotros</h1>
+            <p>
+                Bienvenido a BInmuebles, S.A. Somos una empresa dedicada a la publicación y gestión de bienes inmuebles.
+                Nuestro objetivo es facilitar la compra, venta y alquiler de propiedades, conectando a comisionistas y
+                usuarios interesados en bienes inmuebles de todo tipo.
+            </p>
+
+            <h2>Nuestra Misión</h2>
+            <p>
+                En BInmuebles, nuestra misión es brindar una plataforma confiable y fácil de usar que permita a nuestros
+                comisionistas publicar sus propiedades de manera efectiva y a los usuarios encontrar su lugar ideal.
+                Estamos comprometidos con la transparencia, la integridad y la satisfacción de nuestros clientes.
+            </p>
+
+            <h2>Equipo de Trabajo</h2>
+            <p>
+                Contamos con un equipo de profesionales dedicados y apasionados por el mundo de los bienes inmuebles.
+                Nuestros comisionistas y gerentes están listos para ayudarte a encontrar la propiedad perfecta o a vender
+                la tuya de manera eficiente.
+            </p>
+
         </div>
-    </main>
+    </section>
 </body>
 </html>
 @endsection
